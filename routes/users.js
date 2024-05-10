f85a44faa68227359e97e7675d1eb2f29a07d870
Rishabh Singh <vineetsatya36@gmail.com>
2024-05-10 16:24:57 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,46 @@
+const User = require("../models/User");
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+
+//update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Accout has been Updated");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can only update your account");
+  }
+});
+
+//delete user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Accout has been Deleted");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can only Delete your account");
+  }
+});
+//get a user
+//follow a user
+//unfollow a user
+
+module.exports = router;
